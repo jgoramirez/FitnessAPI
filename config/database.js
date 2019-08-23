e75064d8482d560b67febc4cc662726a858397c4
Jose Guadalupe Omar Ramirez Mata <Jose.Ramirez@municipiodequeretaro.gob.mx>
2019-08-23 16:56:10 -0500

    Version 1 BD

--- config/database.js
+++ config/database.js
@@ -25,6 +25,7 @@ let db = new sqlite.Database(DBSOURCE, (err) => {
                 db.run(insert, ["Technique Guides","Learn amazing street workout and calisthenics","https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06","#A74CF2","#617BFB"])
                 db.run(insert, ["Skills Training","Learn the secrets of bodyweight techniques","https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercises02.png?alt=media&token=a5d55381-5f3e-4f25-92dd-560775f96aa2","#17EAD9","#6078EA"])
                 db.run(insert, ["Strength Training","Train anytime, everywere and become a superhero!","https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise03.png?alt=media&token=8e5301c0-151e-415d-bd2c-655235d9c916","#FAD961","#F76B1C"])
+                db.run(insert, ["Kinesthetic Training","Train taking place, and always enjoy it!","https://image.spreadshirtmedia.net/image-server/v1/mp/designs/145633943,width=178,height=178,version=1550179364/hombre-de-dibujos-animados-mancuerna-ejercicio-ganancia-regalo.png","#00416A","#FFE000"])
             }
         });
     }
